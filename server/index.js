7b7b735e6682cabc388e4b7a9ebd8c50f130418b
Timothy Booser <tbooser@utexas.edu>
2016-03-05 15:03:17 -0600

    new backend setup

--- server/index.js
+++ server/index.js
@@ -1,64 +1,44 @@
 var browserify = require('browserify-middleware');
-var babelify = require('babelify');
 var express = require('express');
 var Path = require('path');
-var sass = require('node-sass-endpoint');
+var Reactify = require('reactify');
 var routes = express.Router();
-var port = process.env.PORT || 4000;
-var app = express();
-var assetFolder = Path.resolve(__dirname, '../client/public');
 
-//This handles todo requests.
-var todoRouter = require('./apis/todo-api.js');
-routes.use('/api/todos', todoRouter)
+routes.get('/app-bundle.js', // <<< ENSURE THIS IS CORRECT FILE
+  browserify('./client/app.js', {
+    transform: [Reactify]
+  }));
 
-routes.get('/api/todo-test', function(req, res) {
-  console.log("In server")
-  res.status(200).send(['node', 'express', 'browserify', 'mithril'])
+// routes.get('/api/example', function(req, res) {
+//   res.send(['node', 'express', 'browserify', 'react', 'react-dom']);
+// });
 
-});
-
-browserify.settings({
-  transform: ['babelify'],
-});
-
-// Provide browserified files at a specified paths
-routes.get('/app-bundle.js',
-  browserify('./client/app.js'));
-
-routes.get('/css/app-bundle.css',
-  sass.serve('./client/public/sass/app.sass'));
-
-// Static assets (html, etc.)
+var assetFolder = Path.resolve(__dirname, '../client/public'); // <<< ENSURE THIS IS CORRECT LOCATION
 routes.use(express.static(assetFolder));
 
 
-if (process.env.NODE_ENV !== 'test') {
-  /**
-    * The Catch-all Route
-    * This is for supporting browser history pushstate.
-    * NOTE: Make sure this route is always LAST.
-  */
-  routes.get('/*', (req, res) => {
-    res.sendFile(`${assetFolder}/index.html`);
+if (process.env.NODE_ENV !== 'test') {   // Development mode
+           
+  var app = express();
+
+  app.use('/', routes);
+
+  app.post('/ENDPOINT HERE', function (request, response) {
+
   });
 
-  /**
-    * We're in development or production mode;
-    * create and run a real server.
-  */
+  // The Catch-all Route. Make sure this route is last.
+  routes.get('/*', function(req, res){
+    console.log('catch-all route triggered');
+    res.sendFile( assetFolder + '/index.html' );
+  });
 
-  // Parse incoming request bodies as JSON
-  app.use( require('body-parser').json() )
-
-  // Mount our main router
-  app.use('/', routes);
-  
- 
-
-  // Start the server!
+  // Start the server
+  var port = process.env.PORT || 4000;
   app.listen(port);
-} else {
-  // We're in test mode; make this file importable instead.
+  console.log("Listening on port", port);
+}
+
+else {                                  // Test mode
   module.exports = routes;
 }
