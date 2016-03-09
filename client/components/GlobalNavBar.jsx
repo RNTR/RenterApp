8c383e4841687341d2dd74fcea66845b13541119
Timothy Booser <tbooser@utexas.edu>
2016-03-09 11:54:20 -0600

    added more skeleton stuff

--- client/components/GlobalNavBar.jsx
+++ client/components/GlobalNavBar.jsx
@@ -4,11 +4,23 @@ var ReactDOM = require('react-dom');
 
 var GlobalNavBar = React.createClass({
 
+signup: function(){},
+
+login: function(){},
+
+logout: function(){},
+
+searchbar: function(){},
+
+listItem: function(){},
+
+titleHome: function(){},  // This is only for linking back to the home page
+
 render: function(){
-	return (  )
+	return (  );
 }
 
-})
+});
 
 
 
--- client/components/UserPage.jsx
+++ client/components/UserPage.jsx
@@ -4,13 +4,19 @@ var ReactDOM = require('react-dom');
 
 var UserPage = React.createClass({
 
+navBar: function(){}, //load globalNavBar;
 
+getInitialState: function(){
+	return {
+		username: {} // this might need to refer to window.globalStateUserId to see what user is currently logged in. will need to check on this.
+	};
+},
 
 render: function(){
-	return (/*DIV NAME HERE*/)
+	return (/*DIV NAME HERE*/);
 }
 
-})
+});
 
 
 
