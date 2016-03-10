c070be09bb458370d2b694327c71ca27914ca3f7
Timothy Booser <tbooser@utexas.edu>
2016-03-09 20:06:19 -0600

    stopped at userExists function

--- db/dbMethods.js
+++ db/dbMethods.js
@@ -21,3 +21,29 @@ exports.addUser = function(username, password, email){
 }
 
 
+exports.userExists = function(id){
+	return new Promise(function(fulfill, reject){
+		knex.select('*').from('users').where('id', id)
+		.then(function(response){
+			//if it gets a response back, and ..., return true
+			//response is [{id:'blah', username: 'blah' etc.}] when found
+			if (response === []){
+				fulfill(false);
+			} else if(response[0].id){
+				fulfill(true);
+			} else {
+				throw new Error({message: "userExists failed when querying."})//continue here!
+			}
+			//response is [] when not found
+			// fulfill(true);
+		})
+		.catch(function(err){
+			reject(err);
+		})
+
+	})
+
+}
+
+exports.userExists(2);
+
