ae03f020685242c418ab0455642430e9989c0c81
Brad Fincher <matrixv01@gmail.com>
2016-03-12 16:43:24 -0600

    Delete duplicate app.js

--- client/App.jsx
+++ client/App.jsx
@@ -1,34 +1,17 @@
-var react = require('react');
-var reactDOM = require('react-dom');
-var Router = require('react-router')
-var GlobalNavBar = require('./components/GlobalNavBar')
-// add dependencies for places where data is being rendered.
-// for example, we will probably be using something like
-// var GlobalNavBar = require('./components/GlobalNavBar.jsx')
+var React = require('react');
+var ReactDOM = require('react-dom');
+var GlobalNavBar = require('./components/GlobalNavBar.jsx')
 
 
-var App = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <GlobalNavBar /> // Persistent NavBar
-        {this.props.children} // Populates routes
-      </div>
-    );
-  }
+
+
+var navBar = React.createElement(GlobalNavBar, {
+	// signin: postRequests.signIn, //(if we do it this way, we will also have to declare variables somewhere on this page requiring the page where the postRequests are made. )
+	// signup: postRequest.signUp
 })
 
 
-
-
-
-// var navBar = React.createElement(GlobalNavBar, {
-// 	signin: postRequests.signIn //(if we do it this way, we will also have to declare variables somewhere on this page requiring the page where the postRequests are made. )
-// 	signup: postRequest.signUp
-// })
-//
-//
-// navBar = ReactDOM.render(navBar, document.getElementById('app'))
+navBar = ReactDOM.render(navBar, document.getElementById('app'))
 
 
 
@@ -42,3 +25,5 @@ var App = React.createClass({
 // example would be -
 
 	// componentVariableName = ReactDOM.render(componentVariableName, document.getElementById('where-this-goes-in-DOM'))
+
+	
