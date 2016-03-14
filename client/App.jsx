84dbe9aa3abaf550ab3450f7f5ced0be07426d0a
Timothy Booser <tbooser@utexas.edu>
2016-03-14 16:52:13 -0500

    working on creating separate pages

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,7 @@ var GlobalNavBar = require('./components/GlobalNavBar.jsx');
 var HomePage = require('./components/HomePage.jsx');
 var UserPage = require('./components/UserPage.jsx');
 
+/***********************************************************/
 
 
 var NavBar = React.createElement(GlobalNavBar, {
@@ -14,36 +15,32 @@ var Home = React.createElement(HomePage, {
   // navbar: GlobalNavBar.render
 });
 
+var User = React.createElement(UserPage, {
+  navBar: GlobalNavBar.render
+});
 
+
+/***********************************************************/
+
+
+
+
+
+
+
+
+/***********************************************************/
 
 
 NavBar = ReactDOM.render(NavBar, document.getElementById('globalnavbar'));
 
 Home = ReactDOM.render(Home, document.getElementById('app'));
 
-
-
-
-
-
-
-
-var User = React.createElement(UserPage, {
-  navBar: GlobalNavBar.render
-});
-
-
 User = ReactDOM.render(User, document.getElementById('user'))
 
 
-// We will also use ReactDOM.render here. From the tyler mcginnis tutorial -
+/***********************************************************/
 
-	// ReactDOM.render takes in two arguments. The first argument is
-	// the component you want to render, the second argument is the
-	// DOM node where you want to render the component.
 
-// example would be -
 
-	// componentVariableName = ReactDOM.render(componentVariableName, document.getElementById('where-this-goes-in-DOM'))
 
-	
