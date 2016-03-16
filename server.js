e43eaf35c1a9dbf3226ac76d06c4f1ee8b8f89f0
Justin Wayne <wayne.jch@gmail.com>
2016-03-16 14:56:35 -0500

    minor change.

--- server.js
+++ server.js
@@ -133,6 +133,13 @@ routes.post('/items/user/rented_from', function (req, res){
 
 routes.delete('/items', function (req, res){
   // delete an item.
+  helpers.deleteItemRoute(req.body)
+    .then(function(response){
+      res.status(200).send(response)
+    })
+    .catch(function(err){
+      res.status(500).send(err);
+    })
 })
 
 
