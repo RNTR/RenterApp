b8d370d13ecb388a9a38e55be77889efd1b8cec5
Brad Fincher <matrixv01@gmail.com>
2016-03-16 10:24:18 -0500

    Rendering test

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -1,12 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router');
 var Link = Router.Link
 
 var SearchResults = React.createClass({
 
   render: function() {
     return (
-      <div>TEST</div>
+      <div>TEST!!!!</div>
     )
   }
 });
