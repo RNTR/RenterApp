0cb00f8c26621e6bd186e304b5b25abbc0a5817f
Justin Wayne <wayne.jch@gmail.com>
2016-03-19 11:58:29 -0500

    added a 'sessions' table to database schema. added a 'description' field to the 'items' table.

--- migrations/20160305170830_RNTR.js
+++ migrations/20160305170830_RNTR.js
@@ -18,6 +18,7 @@ exports.up = function(knex, Promise) {
             table.integer('zip').notNullable();
             table.string('category').notNullable();
             table.integer('price').notNullable();
+            table.string('description').defaultTo('The item owner has not listed any details for this item.');
             table.string('photo').notNullable().defaultTo('http://www.ubeeco.com.au/products/g90eh7mvm.jpg')
             table.integer('item_owner').notNullable()
                  .references('id')
@@ -39,6 +40,14 @@ exports.up = function(knex, Promise) {
             table.dateTime('date_end').notNullable();
             table.string('is_confirmed').notNullable().defaultTo('false');
             table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+        }),
+
+        knex.schema.createTable('sessions', function(table) {
+            table.increments('id').primary();
+            table.integer('user_id').notNullable()
+                .references('id')
+                .inTable('users');
+            table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
         })
     ])
   
@@ -49,7 +58,8 @@ exports.down = function(knex, Promise) {
     return Promise.all([
         knex.schema.dropTable('users'),
         knex.schema.dropTable('items'),
-        knex.schema.dropTable('rentals')
+        knex.schema.dropTable('rentals'),
+        knex.schema.dropTable('sessions')
     ])
 
 };
