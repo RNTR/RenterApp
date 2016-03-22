16e3d7f9c50a7e9fc1a11a0a3fc972af1aa6321e
Brad Fincher <matrixv01@gmail.com>
2016-03-21 19:16:58 -0500

    Create search and zip state handlers

--- client/components/GlobalNavBar.jsx
+++ client/components/GlobalNavBar.jsx
@@ -4,14 +4,30 @@ var Router = require('react-router');
 var Link = Router.Link
 var postRequests = require('../requests/post.js');
 var getRequests = require('../requests/get.js');
-
+import {render} from 'react-dom'
 
 
 var GlobalNavBar = React.createClass({
 
 getInitialState: function(){
-	return {};
+	return {
+		search: '',
+		zip: ''
+	};
 },
+handleSearch: function(e){
+	this.setState({
+		search: e.target.value
+	});
+},
+handleZip: function(e){
+	this.setState({
+		zip: parseInt(e.target.value)
+	})
+},
+submitSearch: function(){
+	
+}
 
 
 login: function(){},
@@ -31,7 +47,11 @@ titleHome: function(){},  // link back to the home page
         <div className="signup" onClick={this.props.signupClick}>Sign Up/Sign In</div>
       	 <div className="listItem" onClick={this.props.addNewItem}>List an Item</div>
       	  <div className="myProfile" onClick={this.props.goToProfile}>My Profile</div>
-      	   <input className='searchbar' placeholder='Search Items' on={this.props.handleSubmit}></input>
+					<form onSubmit={this.submitSearch}>
+						<input className='searchbar' placeholder='Search Items' type="text" value={this.state.search} onChange={this.handleSearch}></input>
+						<input placeholder="Zip Code" type="number" value={this.state.zip} onChange={this.handleZip}></input>
+						<button type="submit">GO</button>
+					</form>
 
       </div>
 
