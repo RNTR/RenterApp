258291848bbd9e70c24e4178a50630b5fc08c87d
Brad Fincher <matrixv01@gmail.com>
2016-03-23 11:53:53 -0500

    Search

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -5,37 +5,21 @@ var Link = Router.Link
 var postRequests = require('../requests/post.js');
 var getRequests = require('../requests/get.js');
 
-
 var SearchResults = React.createClass({
+
+  getInitialState: function() {
+    return {
+      items: ["PS4", "Xbox", "Lawn Mower", "Weed Eater"]
+    }
+  },
 
   render: function() {
     return (
- <div>
-        <div className="results">Search results for location: {postRequests.searchLocation().city[4].toUpperCase()}</div>
-        <div className="resultsContainer">
-          <a href={postRequests.searchResults().imageUrl[0]}>{postRequests.searchResults().item[0]}</a>
-          <div>${postRequests.searchResults().price[0]}/day</div>
-        </div>
-        <div className="resultsContainer">
-          <a href={postRequests.searchResults().imageUrl[1]}>{postRequests.searchResults().item[1]}</a>
-            <div>${postRequests.searchResults().price[1]}/day</div>
-        </div>
-        <div className="resultsContainer">
-          <a href={postRequests.searchResults().imageUrl[2]}>{postRequests.searchResults().item[2]}</a>
-            <div>${postRequests.searchResults().price[2]}/day</div>
-        </div>
-        <div className="resultsContainer">
-          <a href={postRequests.searchResults().imageUrl[3]}>{postRequests.searchResults().item[3]}</a>
-            <div>${postRequests.searchResults().price[3]}/day</div>
-        </div>
+      <div>
+        <div className="newListing">{this.getInitialState().items[3]}</div>
       </div>
-
     )
-	}
+  }
 });
 
 module.exports = SearchResults;
-
-
-
-
